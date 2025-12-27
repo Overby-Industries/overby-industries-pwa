819bb08c9db94efdef07d7cf1ce3776b8a6963b6
Overby-Industries <founder@overbyindustries.space>
2025-12-27 13:37:13 -0600

    Fixed navbar.

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -22,9 +22,26 @@ export default function NavBar() {
             Overby Industries
           </h1>
       </div>
-      <p className="flex flex-col justify-center text-center text-lg text-gray-400 md:flex-row">
-        Aerospace • ISRU • Heavy Industry
-      </p>
+      <nav className="mb-4 flex w-full max-w-4xl flex-wrap items-center justify-center gap-6 px-4 text-lg font-medium text-white">
+      <Link href="/aerospace" className="flex items-center justify-center px-3 py-2 hover:underline">
+        <h2>Aerospace</h2>
+      </Link>
+
+      <Link href="/isru" className="flex items-center justify-center px-3 py-2 hover:underline">
+        <h2>ISRU</h2>
+      </Link>
+
+      <Link href="/heavy-industry" className="flex items-center justify-center px-3 py-2 hover:underline">
+        <h2>Heavy Industry</h2>
+      </Link>
+
+      <Link
+          href="#contribute"
+          className="rounded-lg bg-cyan-500 px-4 py-4 transition hover:bg-cyan-600"
+        >
+          Contact
+        </Link>
+    </nav>
     </div>
   );
 }

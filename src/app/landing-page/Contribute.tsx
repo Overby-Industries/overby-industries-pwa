5a785456acab184efa37212ad8d32ab4e972f2f0
Overby-Industries <founder@overbyindustries.space>
2026-01-15 13:14:45 -0600

    Added new images

--- src/app/landing-page/Contribute.tsx
+++ src/app/landing-page/Contribute.tsx
@@ -8,7 +8,7 @@ export default function Contribute() {
       <h2 className="mb-4 text-3xl font-bold md:text-4xl">Join the Mission</h2>
       <p className="mx-auto mb-4 max-w-2xl text-lg text-gray-300">
         Be part of humanity’s next great leap. Whether as a small donor fueling
-        our first prototypes, or contribute with hands on experience that you bring to the
+        our first prototypes, contribute with hands on experience that you bring to the
         table, or as a major investor partnering for the future, your support
         powers Overby Industries. Every contribution, no matter how small, helps
         us take steps toward a sustainable future in space. Join the early
